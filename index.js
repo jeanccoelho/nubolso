9cb014f4177c0da4c05818ba9654be146aeab87a
Jean Carlos Coelho <jean@iMac-de-Jean.local>
2023-10-30 20:20:56 -0300

    test

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-const app = require('http').createServer();
-const io = require('socket.io')(app);
+//index.js
+const app = require('./app');
+const appWs = require('./app-ws');
 
-io.on('connection', (socket) => {
-  console.log('Um cliente se conectou');
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`App Express is running!`);
+})
 
-  socket.on('mensagemParaServidor', (data) => {
-    console.log('Mensagem do cliente: ' + data);
-    // Aqui você pode manipular a mensagem recebida do cliente
-
-    // Envia uma mensagem de volta para o cliente
-    socket.emit('mensagemParaCliente', 'Mensagem recebida pelo servidor: ' + data);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Um cliente se desconectou');
-  });
-});
-
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor está ouvindo na porta ${PORT}`);
-});
+appWs(server);
